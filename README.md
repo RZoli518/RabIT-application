991e0a731f8c272cc96c7b7197d9f1ac578372ca
ZoltanReviczky <reviczkyzoli@gmail.com>
2022-05-14 22:19:58 +0200

    Home view and README update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RabIT-application
  
+I have no experience in URL mapping, thus after quite a bit of experimentation with the .htaccess I decided to go with a coded navigation in the project due to the time availabe to complete it
--- src/Views/home.php
+++ src/Views/home.php
@@ -13,6 +13,11 @@
                 <div class="head">
                     <h1>RabIT Application</h1>
                 </div>
+                <div class="homecenter">
+                    <p>Created by: Zoltán Reviczky</p>
+                    <p>This site is the task I was given as part of my application to the open intern position</p>
+                    <p>The two buttons below will take you to the two requested pages</p>
+                </div>
                 <div class=navbtns>
                     <a href="http://localhost/RabIT_application/RabIT-application/src/index.php?ad&data=mvc">Advertisements</a>
                     <a href="http://localhost/RabIT_application/RabIT-application/src/index.php?user&data=mvc">Users</a>
